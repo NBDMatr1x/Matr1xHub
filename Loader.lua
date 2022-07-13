b5927c18d91f73f2c30ae1f592eb601e8a11e4ea
Matr1x <77656181+NBDMatr1x@users.noreply.github.com>
2022-07-13 22:42:57 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,85 +4,90 @@ if Matr1x and Matr1x.Loaded then
         Title = "Matr1x Hub",
         Description = "Script already executed!",
         Duration = 5
-    })
-    return
+    }) return
 end
-
-getgenv().Matr1x = {Loaded = false,Debug = false,Current = "Loader",Utilities = {}}
-Matr1x.Utilities.Misc = Matr1x.Debug and loadfile("Matr1xHub/Utilities/Misc.lua")()
-or loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/Utilities/Misc.lua"))()
-Matr1x.Utilities.UI = Matr1x.Debug and loadfile("Matr1xHub/Utilities/UI.lua")()
-or loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/Utilities/UI.lua"))()
-Matr1x.Utilities.Drawing = Matr1x.Debug and loadfile("Matr1xHub/Utilities/Drawing.lua")()
-or loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/Utilities/Drawing.lua"))()
-
-Matr1x.Games = {
-    ["1054526971"] = {
-        Name = "Blackhawk Rescue Mission 5",
-        Script = Matr1x.Debug and readfile("Matr1xHub/Games/BRM5.lua")
-        or game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/Games/BRM5.lua")
-    },
-    ["580765040"] = {
-        Name = "RAGDOLL UNIVERSE",
-        Script = Matr1x.Debug and readfile("Matr1xHub/Games/RU.lua")
-        or game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/Games/RU.lua")
-    },
-    ["1168263273"] = {
-        Name = "Bad Business",
-        Script = Matr1x.Debug and readfile("Matr1xHub/Games/BB.lua")
-        or game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/Games/BB.lua")
-    },
-    ["807930589"] = {
-        Name = "The Wild West",
-        Script = Matr1x.Debug and readfile("Matr1xHub/Games/TWW.lua")
-        or game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/Games/TWW.lua")
-    },
-    ["187796008"] = {
-        Name = "Those Who Remain",
-        Script = Matr1x.Debug and readfile("Matr1xHub/Games/TWR.lua")
-        or game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/Games/TWR.lua")
-    },
-    ["1586272220"] = {
-        Name = "Steel Titans",
-        Script = Matr1x.Debug and readfile("Matr1xHub/Games/ST.lua")
-        or game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/Games/ST.lua")
-    }
-}
 
 local PlayerService = game:GetService("Players")
 local LocalPlayer = PlayerService.LocalPlayer
-local function IfGameSupported()
+
+local function GetSupportedGame() local Game
     for Id, Info in pairs(Matr1x.Games) do
         if tostring(game.GameId) == Id then
-            return Info
+            Game = Info break
         end
-    end
+    end if not Game then
+        Game = Matr1x.Games.Universal
+    end return Game
 end
+
+local function GetScript(Script)
+    return Matr1x.Debug and readfile("Matr1xHub/" .. Script .. ".lua")
+    or game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/" .. Script .. ".lua")
+end
+
+local function LoadScript(Script)
+    return loadstring(Matr1x.Debug and readfile("Matr1xHub/" .. Script .. ".lua")
+    or game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/" .. Script .. ".lua"))()
+end
+
+getgenv().Matr1x = {
+    Loaded = false,
+    Debug = false,
+    Utilities = {},
+    Games = {
+        ["Universal"] = {
+            Name = "Universal",
+            Script = "Universal"
+        },
+        ["1054526971"] = {
+            Name = "Blackhawk Rescue Mission 5",
+            Script = "Games/BRM5"
+        },
+        ["580765040"] = {
+            Name = "RAGDOLL UNIVERSE",
+            Script = "Games/RU"
+        },
+        ["1168263273"] = {
+            Name = "Bad Business",
+            Script = "Games/BB"
+        },
+        ["807930589"] = {
+            Name = "The Wild West",
+            Script = "Games/TWW"
+        },
+        ["187796008"] = {
+            Name = "Those Who Remain",
+            Script = "Games/TWR"
+        },
+        ["1586272220"] = {
+            Name = "Steel Titans",
+            Script = "Games/ST"
+        },
+        ["358276974"] = {
+            Name = "Apocalypse Rising 2",
+            Script = "Games/AR2"
+        }
+    }
+}
+
+Matr1x.Utilities.Misc = LoadScript("Utilities/Misc")
+Matr1x.Utilities.UI = LoadScript("Utilities/UI")
+Matr1x.Utilities.Drawing = LoadScript("Utilities/Drawing")
 
 LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
         local QueueOnTeleport = (syn and syn.queue_on_teleport) or queue_on_teleport
-        QueueOnTeleport(Matr1x.Debug and readfile("Matr1xHub/Loader.lua")
-        or game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/Loader.lua"))
+        QueueOnTeleport(GetScript("Loader"))
     end
 end)
 
-local SupportedGame = IfGameSupported()
+local SupportedGame = GetSupportedGame()
 if SupportedGame then
-    Matr1x.Current = SupportedGame.Name
-    loadstring(SupportedGame.Script)()
+    Matr1x.Game = SupportedGame.Name
+    LoadScript(SupportedGame.Script)
     Matr1x.Utilities.UI:Notification({
         Title = "Matr1x Hub",
-        Description = Matr1x.Current .. " loaded!",
-        Duration = 5
-    }) Matr1x.Loaded = true
-else
-    Matr1x.Current = "Universal"
-    loadstring(Matr1x.Debug and readfile("Matr1xHub/Universal.lua")
-    or game:HttpGetAsync("https://raw.githubusercontent.com/NBDMatr1x/Matr1xHub/main/Universal.lua"))()
-    Matr1x.Utilities.UI:Notification({
-        Title = "Matr1x Hub",
-        Description = Matr1x.Current .. " loaded!",
+        Description = Matr1x.Game .. " loaded!",
         Duration = 5
     }) Matr1x.Loaded = true
 end
